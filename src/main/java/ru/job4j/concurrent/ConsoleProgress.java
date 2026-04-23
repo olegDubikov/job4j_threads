3cfc2257c8c328d8c438c4c31a667e8271e0ad47
олегДубиков <lars75@inbox.ru>
2026-04-23 08:23:31 +0300

    5. Прерывание нити [#1019 #562760]

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -3,22 +3,22 @@ package ru.job4j.concurrent;
 public class ConsoleProgress implements Runnable {
     @Override
     public void run() {
-        Thread progress = new Thread(
-                () -> {
-                    var process = new char[]{'-', '\\', '|', '/'};
-                    while (!Thread.currentThread().isInterrupted()) {
-                        for (char c : process) {
-                            System.out.print("\r load: " + c);
-                            try {
-                                Thread.sleep(500);
-                            } catch (InterruptedException e) {
-                                Thread.currentThread().interrupt();
-                                break;
-                            }
-                        }
-                    }
+        var process = new char[]{'-', '\\', '|', '/'};
+        while (!Thread.currentThread().isInterrupted()) {
+            for (char c : process) {
+                System.out.print("\r load: " + c);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    break;
                 }
-        );
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread progress = new Thread(new ConsoleProgress());
         progress.start();
         try {
             Thread.sleep(5000);
@@ -28,10 +28,5 @@ public class ConsoleProgress implements Runnable {
         progress.interrupt();
         System.out.println();
         System.out.print("...Complete...");
-    }
-
-    public static void main(String[] args) {
-        ConsoleProgress consoleProgress = new ConsoleProgress();
-        consoleProgress.run();
     }
 }
